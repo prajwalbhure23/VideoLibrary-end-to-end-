70a1644bb066be56e652b8011b31a02f2c4fe78a
Prajwal Bhure <137801738+prajwalbhure23@users.noreply.github.com>
2024-08-02 15:42:54 +0530

    Update user-login.jsx

--- frontend/src/components/user-login.jsx
+++ frontend/src/components/user-login.jsx
@@ -30,7 +30,7 @@ export function UserLogin(){
     })
 
     useEffect(()=>{
-        axios.get('https://videolibrary-end-to-end.onrender.com/users')
+        axios.get('https://videolibrary-end-to-end-1.onrender.com/users')
         .then((response)=>{
             setUsers(response.data);
         })
